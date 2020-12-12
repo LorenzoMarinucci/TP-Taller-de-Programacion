37ba53b4db81529baae09fbc1c83abacdffd3b5a
julyvjo <julyvjo@gmail.com>
2020-12-12 18:54:33 -0300

    paquete test

---
+++ Codigo_A/src/test/EscenarioListarFacturasSinFacturas.java
@@ -0,0 +1,17 @@
+package test;
+
+import modelo.Sistema;
+
+public class EscenarioListarFacturasSinFacturas {
+
+	Sistema sistema;
+	
+	public EscenarioListarFacturasSinFacturas() {
+		
+		this.sistema = sistema.getInstancia();
+	}
+	
+	public Sistema getSistema() {
+		return sistema.getInstancia();
+	}
+}
---
+++ Codigo_A/src/test/ListarFacturasTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListarFacturasTest {
+
+	@Before
+	public void setUp() throws Exception {
+		EscenarioListarFacturasSinFacturas escenario1 = new EscenarioListarFacturasSinFacturas();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
